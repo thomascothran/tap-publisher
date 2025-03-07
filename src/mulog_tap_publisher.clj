c2ce546b0a98641f9eea51deeea030b8f6b75fcf
Thomas Cothran <tmcothran@gmail.com>
2025-03-07 11:25:14 -0500

    Initial commit

---
+++ src/mulog_tap_publisher.clj
@@ -0,0 +1,22 @@
+;; ---------------------------------------------------------
+;; Mulog Custom Publishers
+;; - tap publisher for use with Portal and other tap sources
+;; h/t practicalli
+;; ---------------------------------------------------------
+(ns mulog-tap-publisher
+  (:require
+   [com.brunobonacci.mulog.buffer :as mulog-buffer]
+   [portal.api :as p]))
+
+(deftype TapPublisher [buffer transform]
+  com.brunobonacci.mulog.publisher.PPublisher
+  (agent-buffer [_] buffer)
+  (publish-delay [_] 200)
+  (publish [_ buffer]
+    (doseq [item (transform (map second (mulog-buffer/items buffer)))]
+      (tap> item))
+    (mulog-buffer/clear buffer)))
+
+(defn tap
+  [{:keys [transform] :as _config}]
+  (TapPublisher. (mulog-buffer/agent-buffer 10000) (or transform identity)))
